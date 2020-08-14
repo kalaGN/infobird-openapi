12cb09802326b09110968da9a52aa9a8c2c9f69c
kalaGN <491010750@qq.com>
2020-08-14 13:51:47 +0800

    init version

---
+++ README.md
@@ -0,0 +1,6 @@
+# infobird openapi SDK for PHP
+
+## Run environment
+- PHP 5.3+.
+- cURL extension.
+
---
+++ src/openapi/Client.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * @Descripttion: infobird
+ * @version: 1.0
+ * @Author: afei
+ * @Date: 2020-08-14 13:33:11
+ * @LastEditors: afei
+ * @LastEditTime: 2020-08-14 13:43:44
+ */
+namespace Infobird\Openapi;
+
+
+class Client {
+
+
+    public function __construct(){
+            //echo 'hello'
+    }
+
+    public  function auth(){
+        echo 'hello';exit;
+    }
+}
